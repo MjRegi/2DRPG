6571a1922691b7c7526b90ddaad8c44dd2049c45
MjRegi <mjregi1808@gmail.com>
2023-09-09 14:40:04 +0400

    movment finished player model import

--- js/main.js
+++ js/main.js
@@ -1,21 +1,54 @@
 const player = document.getElementById("player");
 // creating player
 
-    let marginDown = 100
-    // creating num for margin top (down)
+
+
+    let marginHorizontal = 0;
+    let marginVertical = 0;
+    let marginChange = 100;
+    // creating num for margins and the change nums
 document.onkeydown = client => {
-    if (client.key == 'w') {
+    if (client.key == 's') {
         moveDown();
+    } else if (client.key == 'w'){
+        moveUp();
+    } else if (client.key == 'd'){
+        moveRight()
+    } else if (client.key == 'a'){
+        moveLeft()
     }
   };
   // keydown events
 
 function moveDown(){
-
-        player.style.marginTop = marginDown + "px"
-        // move down
-
-        marginDown += 100;
-    } 
+    marginHorizontal += marginChange;
+    // adding 100 to marginRight
+    player.style.marginTop = marginHorizontal + "px";
+    // move down
+    console.log(margin);
+} 
 // moving down function
+
+function moveUp(){
+    marginHorizontal += -marginChange;
+    // deleting 100 from marginRight
+    player.style.marginTop = marginHorizontal + "px";
+    // move up
+}
+// moving up function
+
+function moveRight(){
+    marginVertical += marginChange;
+    // adding 100 to marginLeft
+    player.style.marginLeft = marginVertical + "px";
+    // move right
+}
+// moving left function
+
+function moveLeft(){
+    marginVertical += -marginChange;
+    // delete 100 from marginLeft
+    player.style.marginLeft = marginVertical + "px";
+}
+
 // creating movment
